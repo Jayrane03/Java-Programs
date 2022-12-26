b0f2a91c4765db40aeeb2226b6bff6290f1429bd
Jayrane03 <jayu.rane32003@gmail.com>
2022-12-26 14:00:49 +0530

    This is a complete fitness pacakage

---
+++ store.js
@@ -0,0 +1,19 @@
+// console.log("Here is the store")
+// var removerCartItems = document.getElementsByClassName('btn-danger')
+// for(var i=0;i<removerCartItems.length;i++){
+//     var button = removerCartItems[i]
+//     button.addEventListener('click' ,function(){
+//        var buttonClicked = Event.target
+//        buttonClicked.parentElement.parentElement.remove()
+//     })
+// }
+// var removeItems = document.getElementsByClassName("btn-danger")
+
+
+// (function(){
+//     const cartPage = document.getElementsByClassName("cart-page") 
+//     const cartInfo = document.getElementsByClassName("cart-info")
+//     cartInfo.addEventlistener('click' , function(){
+
+//     }); 
+// })();
